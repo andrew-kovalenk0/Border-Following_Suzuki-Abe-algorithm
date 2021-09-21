b3e22f6432627562595b6e575bdf1fd93ffd5edb
After2Dark <43520508+After2Dark@users.noreply.github.com>
2021-09-21 10:30:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import cv2
 def find_clockwise(borders, center, i2j2):
     i = i2j2[0]
     j = i2j2[1]
+
     if center[0] - i == 0 and center[1] - j == -1:
         if borders[i+1, j] != 0:
             return [i+1, j]
@@ -21,54 +22,6 @@ def find_clockwise(borders, center, i2j2):
             return [i-1, j-1]
         if borders[i-1, j] != 0:
             return [i-1, j]
-
-    if center[0] - i == 1 and center[1] - j == -1:
-        if borders[i+1, j] != 0:
-            return [i+1, j]
-        if borders[i+2, j] != 0:
-            return [i+2, j]
-        if borders[i+2, j-1] != 0:
-            return [i+2, j-1]
-        if borders[i+2, j-2] != 0:
-            return [i+2, j-2]
-        if borders[i+1, j-2] != 0:
-            return [i+1, j-2]
-        if borders[i, j-2] != 0:
-            return [i, j-2]
-        if borders[i, j-1] != 0:
-            return [i, j-1]
-
-    if center[0] - i == 1 and center[1] - j == 0:
-        if borders[i, j+1] != 0:
-            return [i, j+1]
-        if borders[i+1, j+1] != 0:
-            return [i+1, j+1]
-        if borders[i+2, j+1] != 0:
-            return [i+2, j+1]
-        if borders[i+2, j] != 0:
-            return [i+2, j]
-        if borders[i+2, j-1] != 0:
-            return [i+2, j-1]
-        if borders[i+1, j-1] != 0:
-            return [i+1, j-1]
-        if borders[i, j-1] != 0:
-            return [i, j-1]
-
-    if center[0] - i == 1 and center[1] - j == 1:
-        if borders[i, j+1] != 0:
-            return [i, j+1]
-        if borders[i, j+2] != 0:
-            return [i, j+2]
-        if borders[i+1, j+2] != 0:
-            return [i+1, j+2]
-        if borders[i+2, j+2] != 0:
-            return [i+2, j+2]
-        if borders[i+2, j+1] != 0:
-            return [i+2, j+1]
-        if borders[i+2, j] != 0:
-            return [i+2, j]
-        if borders[i+1, j] != 0:
-            return [i+1, j]
 
     if center[0] - i == 0 and center[1] - j == 1:
         if borders[i-1, j] != 0:
@@ -86,53 +39,6 @@ def find_clockwise(borders, center, i2j2):
         if borders[i+1, j] != 0:
             return [i+1, j]
 
-    if center[0] - i == -1 and center[1] - j == 1:
-        if borders[i, j+1] != 0:
-            return [i, j+1]
-        if borders[i, j+2] != 0:
-            return [i, j+2]
-        if borders[i-1, j+2] != 0:
-            return [i-1, j+2]
-        if borders[i-2, j+2] != 0:
-            return [i-2, j+2]
-        if borders[i-2, j+1] != 0:
-            return [i-2, j+1]
-        if borders[i-2, j] != 0:
-            return [i-2, j]
-        if borders[i-1, j] != 0:
-            return [i-1, j]
-
-    if center[0] - i == -1 and center[1] - j == 0:
-        if borders[i, j-1] != 0:
-            return [i, j-1]
-        if borders[i-1, j-1] != 0:
-            return [i-1, j-1]
-        if borders[i-2, j-1] != 0:
-            return [i-2, j-1]
-        if borders[i-2, j] != 0:
-            return [i-2, j]
-        if borders[i-2, j+1] != 0:
-            return [i-2, j+1]
-        if borders[i-1, j+1] != 0:
-            return [i-1, j+1]
-        if borders[i, j+1] != 0:
-            return [i, j+1]
-
-    if center[0] - i == -1 and center[1] - j == -1:
-        if borders[i, j-1] != 0:
-            return [i, j-1]
-        if borders[i, j-2] != 0:
-            return [i, j-2]
-        if borders[i-1, j-2] != 0:
-            return [i-1, j-2]
-        if borders[i-2, j-2] != 0:
-            return [i-2, j-2]
-        if borders[i-2, j-1] != 0:
-            return [i-2, j-1]
-        if borders[i-2, j] != 0:
-            return [i-2, j]
-        if borders[i-1, j] != 0:
-            return [i-1, j]
     return None
 
 
@@ -158,7 +64,7 @@ def find_counterclockwise(borders, center, i2j2):
 
     if center[0] - i == 1 and center[1] - j == -1:
         if borders[i, j-1] != 0:
-            return [i, j-1], exam_pixels
+            return [i, j-1], pixel_find
         if borders[i, j-2] != 0:
             return [i, j-2], pixel_find
         if borders[i+1, j-2] != 0:
@@ -352,9 +258,27 @@ def find_borders(img):
 if __name__ == '__main__':
     start_time = time.time()
     image = cv2.imread('image.png', 0)
-    border, num = find_borders(image)
-    np.savetxt('border.txt', border, fmt=f'%{len(str(num)) + 1}d', )
-    border[border < 0] *= -1
-    border[border > 1] *= int(255 / num)
-    cv2.imwrite('border.png', border)
+
+    image[image == 255] = 1
+    mask = np.array([[1, 1, 1],
+                     [1, 0, 1],
+                     [1, 1, 1]]).astype('bool')
+    pixel_list = image[3:6, 3:6][mask].tolist()
+    roll_dict = {
+        (1, 1): 0,
+        (1, 0): 1,
+        (1, -1): 2,
+        (0, 1): 3,
+        (0, -1): 4,
+        (-1, 1): 5,
+        (-1, 0): 6,
+        (-1, -1): 7
+    }
+    print(np.roll(pixel_list, -roll_dict[(1, 0)]))
+
+    # border, num = find_borders(image)
+    # np.savetxt('border.txt', border, fmt=f'%{len(str(num)) + 1}d', )
+    # border[border < 0] *= -1
+    # border[border > 1] *= int(255 / num)
+    # cv2.imwrite('border.png', border)
     print(f'{time.time() - start_time} seconds')
